695c81b74f85ed93f9b18da38f89d6a03fc3d9d7
Christopher Maier <cm@opscode.com>
2013-06-27 13:13:00 -0400

    Initial commit

---
+++ recipes/default.rb
@@ -0,0 +1,25 @@
+#
+# Cookbook Name:: sqitch
+# Recipe:: default
+#
+# Copyright (C) 2013 Opscode, Inc.
+#
+
+include_recipe "perl"
+
+# TODO: Consider using the cpan[1] cookbook instead for more robust
+# installation (specifying versions, installing from an artifact, etc.)
+#
+# [1]: http://community.opscode.com/cookbooks/cpan
+cpan_module "App::Sqitch"
+
+# Map values of node['sqitch']['engine'] to the Perl modules that
+# support them.
+engine_modules = {
+  "pg"     => "DBD::Pg",
+  "sqlite" => "DBD::SQLite",
+  "oracle" => "DBD::oracle"
+}
+
+# Install the engine the user wants
+cpan_module engine_modules[node['sqitch']['engine']]
