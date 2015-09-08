bcbfd8d39b4608e1ab5f2234b191ffebc96857e2
Tim Smith <tsmith84@gmail.com>
2015-09-08 14:01:56 -0700

    Update contacts and add supermarket metadata

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             "sqitch"
-maintainer       "Opscode, Inc."
-maintainer_email "cm@opscode.com"
+maintainer       'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io'
 license          "Apache 2.0"
 description      "Installs sqitch for managing SQL changesets"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
@@ -8,3 +8,6 @@ version          "0.5.0"
 recipe           "sqitch", "Installs sqitch"
 
 depends "perl", ">= 1.0.0"
+
+source_url 'https://github.com/chef-cookbooks/sqitch' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/sqitch/issues' if respond_to?(:issues_url)
