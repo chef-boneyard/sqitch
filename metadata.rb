7a823f24a67fb30fb8061d246f99f5ba265de43f
Tim Smith <tsmith@chef.io>
2017-01-26 14:02:47 -0800

    Testing updates
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -12,5 +12,4 @@ depends 'build-essential'
 
 source_url 'https://github.com/chef-cookbooks/sqitch'
 issues_url 'https://github.com/chef-cookbooks/sqitch/issues'
-
-chef_version '>= 12.1'
+chef_version '>= 12.1' if respond_to?(:chef_version)
